46305b2b0f9b261c470e082650573f79a3f51d6e
Prinyada <prinyadapinn@gmail.com>
2023-07-24 01:28:13 +0700

    checkwin, get,post complete

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import "./App.css";
 import { Board } from "./components/Board";
 import React, { useEffect } from "react";
 import { useState } from "react";
+import axios from 'axios';
 
 function App() {
-  const [board, setBoard] = useState(Array);
+  const [board, setBoard] = useState(Array(9).fill(null));
   const [isClick, setIsClick] = useState(false);
   const [xPlaying, setXplaying] = useState(true);
 
   // save to database
-  const [size, setSize] = useState(3); 
-  const [win, setWin] = useState(""); 
-  const [lose, setLose] = useState(""); 
-  const [toe, setToe] = useState(false); 
-  const [result, setResult] = useState(""); 
+  const [size, setSize] = useState(3);
+  let win;
+  let lose;
+  let toe;
+  let result;
 
-  let allDataHistory = [];
-  let saveWinToDb;
+  let allData = [];
+
+  function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
   const style = {
     display: "grid",
@@ -41,18 +45,18 @@ function App() {
 
   function convertTo2DArray(oneDArray, rows, columns) {
     const twoDArray = [];
-  
+
     // Check if the given rows and columns are valid for the 1D array length
     if (rows * columns !== oneDArray.length) {
       throw new Error("Invalid dimensions for conversion");
     }
-  
+
     for (let i = 0; i < rows; i++) {
       // Get a slice of the 1D array for each row
       const rowSlice = oneDArray.slice(i * columns, (i + 1) * columns);
       twoDArray.push(rowSlice);
     }
-  
+
     return twoDArray;
   }
 
@@ -60,11 +64,12 @@ function App() {
     const oneDArray = [].concat(...twoDArray);
     return oneDArray;
   }
-  
-  
-  function isWinningState(board, symbol) {
-    let tempBoard = board;
-    tempBoard = convertTo2DArray(tempBoard, size, size);
+
+  function isWinningState(updateBoard, symbol) {
+
+    let tempBoard = updateBoard;
+    let newUpdateBoard;
+    tempBoard = convertTo2DArray(tempBoard, size, size); // เอาไว้เช็คเฉยๆ
 
     // Check rows
     for (let row = 0; row < size; row++) {
@@ -75,7 +80,12 @@ function App() {
           break;
         }
       }
-      if (rowWin) return true;
+      if (rowWin) {
+        if(updateBoard.includes(null)){
+          newUpdateBoard = updateBoard.map( n => n === null ? 'N' : n)
+        }
+        return { success: true, data: newUpdateBoard };
+      } 
     }
 
     // Check columns
@@ -87,7 +97,12 @@ function App() {
           break;
         }
       }
-      if (colWin) return true;
+      if (colWin) {
+        if(updateBoard.includes(null)){
+          newUpdateBoard = updateBoard.map( n => n === null ? 'N' : n)
+        }
+        return { success: true, data: newUpdateBoard };
+      }
     }
 
     // Check main diagonal (top-left to bottom-right)
@@ -98,7 +113,12 @@ function App() {
         break;
       }
     }
-    if (mainDiagonalWin) return true;
+    if (mainDiagonalWin) {
+      if(updateBoard.includes(null)){
+          newUpdateBoard = updateBoard.map( n => n === null ? 'N' : n)
+        }
+        return { success: true, data: newUpdateBoard }
+    }
 
     // Check anti-diagonal (top-right to bottom-left)
     let antiDiagonalWin = true;
@@ -108,37 +128,91 @@ function App() {
         break;
       }
     }
-    if (antiDiagonalWin) return true;
+    if (antiDiagonalWin){
+      if(updateBoard.includes(null)){
+        newUpdateBoard = updateBoard.map( n => n === null ? 'N' : n)
+      }
+      return { success: true, data: newUpdateBoard };
+    }
 
-    return false; // No winning state found
+    return { success: false, data: updateBoard }; // No winning state found
   }
 
   const handleBoxClick = (boxIdx) => {
-    
     const updatedBoard = board.map((value, idx) => {
-      if (idx === boxIdx) {
-        return xPlaying === true ? "X" : "O";
-      } else {
+      if(idx === boxIdx){
+        return xPlaying === false ? "X" : "O";
+      }
+      else {
         return value;
       }
     });
     
     setBoard(updatedBoard);
     setXplaying(!xPlaying);
-    
-    let winX = isWinningState(updatedBoard,"X");
-    let winO = isWinningState(updatedBoard,"O");
-    
-    console.log("this winX -> ",winX);
-    console.log("this winO -> ",winO);
 
+    const winX = isWinningState(updatedBoard,"X");
+    const winO = isWinningState(updatedBoard,"O");
+
+    if(winX.success === true){
+      const tempStringX = winX.data.join('');
+      result = tempStringX;
+      win = "x";
+      lose = "o";
+      toe = "-";
+      addData(win, lose, toe, result, size);
+    }
+    else if(winO.success === true){
+      const tempStringO = winO.data.join('');
+      result = tempStringO;
+      win = "o";
+      lose = "x";
+      toe = "-";
+      addData(win, lose, toe, result, size);
+    }
+    else if(!winX.data.includes('N') && !winO.data.includes('N') && !winX.data.includes(null) && !winO.data.includes(null) && (winX.success === false) && (winO.success === false)){
+      const tempStringXO = winX.data.join('');
+      result = tempStringXO;
+      win = "-";
+      lose = "-";
+      toe = "toe";
+      addData(win, lose, toe, result, size);
+    }
   };
 
+  async function addData(win, lose, toe, result, size){
+    await axios.post('http://localhost:8000/data', {
+      win: win,
+      lose: lose,
+      toe: toe,
+      result: result,
+      size: size
+    });
+
+    allData.push({
+      win: win,
+      lose: lose,
+      toe: toe,
+      result: result,
+      size: size
+    });
+    await wait(3000);
+    createBoard();
+  }
+
+  async function getData(){
+    const data = await axios.get('http://localhost:8000/data', { crossdomain: true })
+    .then((response) => {
+      return response.data.data;
+    })
+    allData.push(...data);
+    console.log("this allData -> ",allData);
+  }
 
   useEffect(() => {
     createBoard();
-  }, []);
-
+  },[]); 
+  
   return (
     <div className="App">
       <div className="board">
@@ -146,6 +220,8 @@ function App() {
         <input onChange={handleValueChange} />
         &nbsp;
         <button onClick={createBoard}>OK</button>
+        &nbsp;
+        <button onClick={getData}>history</button>
       </div>
       <div className="board-size">
         {size} x {size}
